939c4a347be05af8767250d95c230bede5d1d9c7
fieldhunter <1677532160@qq.com>
2020-03-10 22:30:03 +0800

    perfect data augmention

--- train.py
+++ train.py
@@ -172,7 +172,12 @@ def data_generator(annotation_lines, batch_size, input_shape, anchors, num_class
         for b in range(batch_size):
             if i==0:
                 np.random.shuffle(annotation_lines)
-            image, box = get_random_data(annotation_lines[i], input_shape, random=True)
+            image, box = get_random_data(annotation_lines[i], input_shape)
+            while type(image) == type(None):
+                i = (i+1) % n
+                if i==0:
+                    np.random.shuffle(annotation_lines)
+                image, box = get_random_data(annotation_lines[i], input_shape)
             image_data.append(image)
             box_data.append(box)
             i = (i+1) % n
--- yolo3/utils.py
+++ yolo3/utils.py
@@ -3,10 +3,14 @@
 from functools import reduce
 from PIL import Image
 import numpy as np
-from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
 import cv2
-import albumentations as A
 import random
+from albumentations import (
+    HorizontalFlip,
+    VerticalFlip,
+    RandomCrop,
+    Compose
+)
 
 import sys
 sys.path.insert(0,'../data_augmention/')
@@ -39,63 +43,97 @@ def letterbox_image(image, size):
 def rand(a=0, b=1):
     return np.random.rand()*(b-a) + a
 
-def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True):
-    '''random preprocessing for real-time data augmentation'''
+def get_aug(aug, min_area=0., min_visibility=0.):
+    return Compose(aug, bbox_params={'format': 'pascal_voc', 
+                                     'min_area': min_area, 
+                                     'min_visibility': min_visibility,
+                                     'label_fields': ['category_id']})
+
+def get_random_data(annotation_line, input_shape):
+    # 读入图片，候选框和尺寸信息
     line = annotation_line.split()
     image = cv2.imread(line[0])
     ih, iw = image.shape[:2]
     h, w = input_shape
     box = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
 
-    # resize image
+    # 候选框和标签分离
+    bboxes = box.tolist()
+    category_id = []
+    for n in range(len(bboxes)):
+        category_id.append(bboxes[n][-1])
+        del bboxes[n][-1]
+
+    # 直接缩放或者随机裁剪来缩放图片，目的是保持图片原有的长宽比
     scale = min(w/iw, h/ih)
     nw = int(iw*scale)
     nh = int(ih*scale)
-    image = cv2.resize(image, (nw,nh))
+    if rand() < .5:
+        annotations = {'image': image,
+                       'bboxes':bboxes,
+                       'category_id': category_id}
+        aug = get_aug([Resize(p=1, height=nh, width=nw)])
+        augmented = aug(**annotations)
+
+        # 删除面积过小的候选框
+        bboxes = augmented['bboxes']
+        category_id = augmented['category_id']
+        for n in range(len(bboxes)):
+            if n >= len(bboxes):
+                break
+
+            i = bboxes[n]
+            if (i[2]-i[0]) * (i[3]-i[1]) < 120:
+                del bboxes[n]
+                del category_id[n]
+        if len(bboxes) == 0:
+            return None, None
+
+        # 图像颜色还原
+        augmented['bboxes'] = bboxes
+        augmented['category_id'] = category_id
+        augmented['image'] = augment(augmented['image'])
+    else:
+        annotations = {'image': image,
+                       'bboxes':bboxes,
+                       'category_id': category_id}
+        aug = get_aug([RandomCrop(p=1, height=nh, width=nw)], 
+                       min_visibility=0.3)
+        augmented = aug(**annotations)
+        if len(augmented['bboxes']) == 0:
+            return None, None
+        # 图像颜色还原
+        augmented['image'] = augment(augmented['image'])
+
+    # 水平和垂直翻转
+    if rand() < .5:
+        aug = get_aug([VerticalFlip(p=1)])
+        augmented = aug(**augmented)
+    if rand() < .5:
+        aug = get_aug([HorizontalFlip(p=1)])
+        augmented = aug(**augmented)
+
+    # 标签数组和候选框数组合并
+    image = augmented['image']
+    box = augmented['bboxes']
+    category_id = augmented['category_id']
+    for n in range(len(box)):
+        box[n] = list(map(lambda x : round(x), box[n]))
+        box[n].append(category_id[n])
     
-    image = augment(image)
-    dx = (w-nw)//2
-    dy = (h-nh)//2
-    box[:, [0,2]] = box[:, [0,2]]*scale + dx
-    box[:, [1,3]] = box[:, [1,3]]*scale + dy
-
-
-    # # flip image or not
-    # flip = rand()<.5
-    # if flip: image = image.transpose(Image.FLIP_LEFT_RIGHT)
-
-    # # distort image
-    # hue = rand(-hue, hue)
-    # sat = rand(1, sat) if rand()<.5 else 1/rand(1, sat)
-    # val = rand(1, val) if rand()<.5 else 1/rand(1, val)
-    # x = rgb_to_hsv(np.array(image)/255.)
-    # x[..., 0] += hue
-    # x[..., 0][x[..., 0]>1] -= 1
-    # x[..., 0][x[..., 0]<0] += 1
-    # x[..., 1] *= sat
-    # x[..., 2] *= val
-    # x[x>1] = 1
-    # x[x<0] = 0
-    # image_data = hsv_to_rgb(x) # numpy array, 0 to 1
-
-    
-    image = np.pad(image, ((dy, dy), (dx, dx), (0, 0)), 'constant', constant_values=128)
+    # 用灰色像素块来做背景扩充图片满足输入尺寸需求
+    dx = (w-nw) // 2
+    dy = (h-nh) // 2
+    image = np.pad(image, ((dy, dy), (dx, dx), (0, 0)),
+                   'constant', constant_values=128)
     if tuple(image.shape[:2]) != input_shape:
-        image = np.pad(image, ((0, input_shape[0]-image.shape[0]), (0, input_shape[1]-image.shape[1]), (0, 0)),\
-            'constant', constant_values=128)
+        image = np.pad(image, ((0, input_shape[0]-image.shape[0]), 
+                (0, input_shape[1]-image.shape[1]), (0, 0)),
+                'constant', constant_values=128)
 
-    # correct boxes
-    # box_data = np.zeros((max_boxes,5))
-    # if len(box)>0:
-    #     np.random.shuffle(box)
-    #     if flip: box[:, [0,2]] = w - box[:, [2,0]]
-    #     box[:, 0:2][box[:, 0:2]<0] = 0
-    #     box[:, 2][box[:, 2]>w] = w
-    #     box[:, 3][box[:, 3]>h] = h
-    #     box_w = box[:, 2] - box[:, 0]
-    #     box_h = box[:, 3] - box[:, 1]
-    #     box = box[np.logical_and(box_w>1, box_h>1)] # discard invalid box
-    #     if len(box)>max_boxes: box = box[:max_boxes]
-    #     box_data[:len(box)] = box
+    image = image / 255.
+    box = np.array([np.array(box[i]) for i in range(len(box))])
+    box[:, [0,2]] = box[:, [0,2]] + dx
+    box[:, [1,3]] = box[:, [1,3]] + dy
 
-    return image_data, box
+    return image, box
